a8e46c27ecb10dae876c17fdaaa6cc7f8223b490
Elmosius <2272008@maranatha.ac.id>
2025-09-16 13:36:16 +0700

    feat: install lib iconify

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import { Button } from '../../ui/button';
+import { Icon } from '@iconify/react';
 
 const Home = () => {
   return (
     <>
       <Button variant={'outline'}>Click me</Button>
+      <Icon icon='mdi-light:home' />
     </>
   );
 };
