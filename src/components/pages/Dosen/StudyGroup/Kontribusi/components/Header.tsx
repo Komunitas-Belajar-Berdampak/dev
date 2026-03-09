d1fa5f8748f8c8239cc930383c2f2753727ff554
Elmosius <2272008@maranatha.ac.id>
2026-03-09 15:33:03 +0700

    feat: update header dashboard kontribusi kalau belum ada point

--- src/components/pages/Dosen/StudyGroup/Kontribusi/components/Header.tsx
+++ src/components/pages/Dosen/StudyGroup/Kontribusi/components/Header.tsx
@@ -10,27 +10,29 @@ type KontribusiHeaderProps = {
 };
 
 const KontribusiHeader = ({ namaAnggota, data, totalKontribusi }: KontribusiHeaderProps) => {
+  console.info(data);
   return (
     <ContentHeader>
       <h1 className='text-primary font-bold text-lg '>{namaAnggota}</h1>
 
-      {data.map((e) => (
-        <div className='flex flex-row gap-6 items-center mt-4 w-full px-4 ' key={e.thread}>
-          <Circle size={12} className='flex justify-center items-center'>
-            <ListChecks className='text-primary w-12 h-12 p-3.5' />
-          </Circle>
+      {data.length === 0 ? (
+        <p className='text-accent text-xs'> Belum ada point pada thread apapun </p>
+      ) : (
+        data.map((e) => (
+          <div className='flex flex-row gap-6 items-center mt-4 w-full px-4 ' key={e.thread}>
+            <Circle size={12} className='flex justify-center items-center'>
+              <ListChecks className='text-primary w-12 h-12 p-3.5' />
+            </Circle>
 
-          <span className='text-primary font-bold text-sm w-full'>{e.thread}</span>
+            <span className='text-primary font-bold text-sm w-full'>{e.thread}</span>
 
-          <div className='h-[0.4rem] bg-accent overflow-hidden rounded-xl w-1/3'>
-            <div className='bg-primary h-full rounded-full' style={{ width: `${(e.kontribusi / totalKontribusi) * 100}%` }} />
+            <div className='h-[0.4rem] bg-accent overflow-hidden rounded-xl w-1/3'>
+              <div className='bg-primary h-full rounded-full' style={{ width: `${(e.kontribusi / totalKontribusi) * 100}%` }} />
+            </div>
+            <span className='text-primary font-bold text-sm'>{e.kontribusi}</span>
           </div>
-          <span className='text-primary font-bold text-sm'>{e.kontribusi}</span>
-        </div>
-        
-      ))}
-
-      
+        ))
+      )}
     </ContentHeader>
   );
 };
