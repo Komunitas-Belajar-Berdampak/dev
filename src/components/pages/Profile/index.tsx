3c2550a087fcb2883c07c690a59a7a3624ffa73f
Elmosius Suli <elmosius18@gmail.com>
2026-04-13 16:32:47 +0700

    feat: change layout profile

--- src/components/pages/Profile/index.tsx
+++ src/components/pages/Profile/index.tsx
@@ -1,27 +1,29 @@
-import Title from "@/components/shared/Title";
-import ProfileContent from "./components/ProfileContent";
-import PublicFiles from "./components/PublicFiles";
-import { useParams } from "react-router-dom";
-import { useFetchProfile } from "./hooks/useFetchProfile";
-import type { UserProfile } from "@/types/profile";
+import Title from '@/components/shared/Title';
+import type { UserProfile } from '@/types/profile';
+import { useParams } from 'react-router-dom';
+import ProfileContent from './components/ProfileContent';
+import PublicFiles from './components/PublicFiles';
+import { useFetchProfile } from './hooks/useFetchProfile';
 
 const ProfilePage = () => {
   const breadcrumbItems = [
     {
-      label: "Profile",
-      href: "/profile",
+      label: 'Profile',
+      href: '/profile',
     },
   ];
   const { id } = useParams();
   const { data, isPending } = useFetchProfile(id);
   return (
-    <section className="flex flex-col gap-[35px]">
-      <Title title="Profile" items={breadcrumbItems} />
-      <ProfileContent
-        isEditing={false}
-        data={data as UserProfile}
-        isPending={isPending}
-      />
+    <section
+      className=' w-full
+        max-w-[1400px]
+        mx-auto
+        px-4 sm:px-6 lg:px-8
+        space-y-6 sm:space-y-8'
+    >
+      <Title title='Profile' items={breadcrumbItems} />
+      <ProfileContent isEditing={false} data={data as UserProfile} isPending={isPending} />
       <PublicFiles id={data?.id as string} />
     </section>
   );
