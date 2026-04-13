05f550c353a9c2e8c312c12da1a57c64668ef9f0
Elmosius Suli <elmosius18@gmail.com>
2026-04-13 14:12:43 +0700

    feat: edit sidebar logo and title name

--- src/components/shared/AppSidebar/index.tsx
+++ src/components/shared/AppSidebar/index.tsx
@@ -17,8 +17,8 @@ const AppSidebar = () => {
           <SidebarMenuItem>
             <SidebarMenuButton asChild size={'lg'}>
               <Link to={'/'} className='flex gap-3'>
-                <div className='size-8 flex justify-center items-center rounded-md bg-primary' />
-                <span className='font-bold text-xl md:group-data-[collapsible=icon]:hidden'>KBB</span>
+                <img src='/img/logo.png' alt='logo komunitas belajar berdapampak' className='size-8 flex justify-center items-center rounded-md bg-primary' />
+                <span className='font-bold text-[10px] md:group-data-[collapsible=icon]:hidden'>Komunitas Belajar Berdampak</span>
               </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
