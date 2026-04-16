489e6872776accb214ec95eda24f32713ecc0baf
Elmosius Suli <elmosius18@gmail.com>
2026-04-16 10:45:33 +0700

    chore: update style layout on edit profile

--- src/components/pages/Profile/Edit/index.tsx
+++ src/components/pages/Profile/Edit/index.tsx
@@ -1,31 +1,33 @@
-import Title from "@/components/shared/Title";
-import ProfileContent from "../components/ProfileContent";
-import { useParams } from "react-router-dom";
-import { useFetchProfile } from "../hooks/useFetchProfile";
-import type { UserProfile } from "@/types/profile";
+import Title from '@/components/shared/Title';
+import type { UserProfile } from '@/types/profile';
+import { useParams } from 'react-router-dom';
+import ProfileContent from '../components/ProfileContent';
+import { useFetchProfile } from '../hooks/useFetchProfile';
 
 const EditProfilePage = () => {
   const breadcrumbItems = [
     {
-      label: "Profile",
-      href: "/profile",
+      label: 'Profile',
+      href: '/profile',
     },
     {
-      label: "Edit Profile",
-      href: "/profile/edit",
+      label: 'Edit Profile',
+      href: '/profile/edit',
     },
   ];
   const { id } = useParams();
   const { data, isPending } = useFetchProfile(id);
 
   return (
-    <section className="flex flex-col gap-[35px]">
-      <Title title="Edit Profile" items={breadcrumbItems} />
-      <ProfileContent
-        isEditing={true}
-        data={data as UserProfile}
-        isPending={isPending}
-      />
+    <section
+      className='w-full
+        max-w-[1400px]
+        mx-auto
+        px-4 sm:px-6 lg:px-8
+        space-y-6 sm:space-y-8'
+    >
+      <Title title='Edit Profile' items={breadcrumbItems} />
+      <ProfileContent isEditing={true} data={data as UserProfile} isPending={isPending} />
     </section>
   );
 };
